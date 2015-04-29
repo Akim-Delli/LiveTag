2cfc79e7b63849105aeb64fb3608db6df15352e6
Akim Delli <adelli@liveintent.com>
2015-04-28 23:01:27 -0400

    random serving ad

--- livetag/routes/index.js
+++ livetag/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
+
+router.get('/ads', function(req, res, next) {
+    pictureNumber = getRandomArbitrary(1, 12);
+    var img = fs.readFileSync('./public/images/' + pictureNumber + '.jpeg');
+    res.writeHead(200, {'Content-Type': 'image/jpeg' });
+    res.end(img, 'binary');
+});
+
+// Returns a random number between min (inclusive) and max (exclusive)
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
 
 module.exports = router;
